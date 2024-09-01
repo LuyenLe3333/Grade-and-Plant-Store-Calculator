b2c641e8c8a6a42980a231ca4b2c068425e03fe1
Luyen Le <156025572+LuyenLe3333@users.noreply.github.com>
2024-09-01 15:37:27 -0500

    Update lle00Lab1a.java

--- lle00Lab1a.java
+++ lle00Lab1a.java
@@ -8,39 +8,33 @@ public class lle00Lab1a {
 	public static void main(String[] args) {
 		double earned = 786.0;
 		double soFar = 950.0;
-		double currPercent = earned/soFar;
+		double currPercent = currPercent(earned, soFar);
 		double A = 0.90;
 		double B = 0.80;
 		double C = 0.70;
 		double D = 0.60;
 		
-		System.out.println("Pat has earned " + earned + " points out of " + soFar + " points possible so far.");
-		System.out.printf("Current percentage earned is " + "%,.2f",currPercent);
+		System.out.println("Pat has earned " + earned + "/" + soFar + " points possible so far.");
+		System.out.printf("Current percentage earned: " + "%,.2f",currPercent);
 		System.out.println("%");
 		
 		if(currPercent > A) {
-			System.out.println("Current grade is A");
+			System.out.println("Current grade: A");
 		} else if(currPercent > B) {
-			System.out.println("Current grade is B");
+			System.out.println("Current grade: B");
 		} else if(currPercent > C) {
-			System.out.println("Current grade is C");
+			System.out.println("Current grade: C");
 		} else {
-			System.out.println("Current grade is D");
+			System.out.println("Current grade: D");
 		}
 		
 		System.out.println();
 		
-		double pointsleftforA = (A * soFar) - earned;
-		double pointsleftforB = (B * soFar) - earned;
-		double pointsleftforC = (C * soFar) - earned;
-		double pointsleftforD = (D * soFar) - earned;
-		
-		
 		System.out.println("To earn this grade, Pat needs to earn this many points (of the remaining)");
-		System.out.printf("        A          " + "%,.1f\n",pointsleftforA);
-		System.out.printf("        B          " + "%,.1f\n",pointsleftforB);
-		System.out.printf("        C          " + "%,.1f\n",pointsleftforC);
-		System.out.printf("        D          " + "%,.1f\n",pointsleftforD);
+		System.out.printf("        A          %.0f\n", calcRemaining(A, soFar, earned));
+		System.out.printf("        B          %.0f\n", calcRemaining(B, soFar, earned));
+		System.out.printf("        C          %.0f\n", calcRemaining(C, soFar, earned));
+		System.out.printf("        D          %.0f\n", calcRemaining(D, soFar, earned));
 		
 		/*
 		 ** I don't remember if this was original prompt in instructions **
@@ -49,7 +43,13 @@ public class lle00Lab1a {
 		System.out.println("        C           264");
 		System.out.println("        D           114");
 		*/
-		
+	}
+	
+	public static double currPercent(double earned, double soFar) {
+		return earned/soFar;
 	}
 
+	public static double calcRemaining(double letter, double soFar, double earned) {
+		return (letter * soFar) - earned;
+	}
 }
